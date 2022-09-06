1d1c8d5184a0ec9e0a67d93b24462c09f99e29ed
Scribee <c1sloan6@gmail.com>
2022-09-06 09:41:26 -0700

    Create empty game manager script

---
+++ Assets/src/Carson/GameManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Class to control UI elements and game event execution.
+public class GameManager : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
