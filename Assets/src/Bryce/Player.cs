72fb336f2a56142f87edd734f70affabe568547e
EndGhost214 <112347364+EndGhost214@users.noreply.github.com>
2022-09-28 20:41:27 -0700

    Added a player->survivalplayer script and a player prefab

---
+++ Assets/src/Bryce/Player.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+
+   
+    
+    private void FixedUpdate()
+    {
+        //movment in here
+    }
+}
---
+++ Assets/src/Bryce/SurvivalPlayer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurvivalPlayer : Player
+{
+
+    [SerializeField]
+    private GameObject SrvPlayer;
+
+    public SurvivalPlayer()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
