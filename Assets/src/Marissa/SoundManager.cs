a66e4914a128b4a6aabb9f8882923117bb3a11a6
mj3272 <marissasamayoa2@gmail.com>
2022-09-28 20:03:46 -0700

    added zombie sound

---
+++ Assets/src/Marissa/SoundManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+  
+
+    [SerializeField]
+    private AudioSource zombieSound1;
+    //Player
+
+    public void ZombieSoundFunction(){
+        zombieSound1.Play();
+
+    }
+
+
+    //ZombieDog
+
+
+
+
+
+}
