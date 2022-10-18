693109e1f4400107b277a6151fa59957a7b60263
mj3272 <marissasamayoa2@gmail.com>
2022-10-17 21:10:45 -0700

    changed

--- Assets/src/Marissa/ItemDrop.cs
+++ Assets/src/Marissa/ItemDrop.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemDrop : MonoBehaviour
+public class ItemDrop : Singleton
 {
 
     private Rigidbody2D itemRb;
