ca00c0d8310245f8f815764b8c546bebd44d051a
MollyEM <mollymeadows584@yahoo.com>
2022-10-26 13:57:57 -0700

    untracked files

---
+++ Assets/tst/Bryce/Tests/Health_tests.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+public class Health_tests : MonoBehaviour
+{
+
+
+
+    [Test]
+    public void checkPlayerHealthisFull()
+    {
+        SurvivalPlayer bob = new SurvivalPlayer(new Player());
+        uint check;
+        check = bob.GetHealth();
+        Assert.AreEqual(check, 100);
+    }
+
+    [Test]
+    public void checkPlayerHealthAfterDamage()
+    {
+
+        SurvivalPlayer bob = new SurvivalPlayer(new Player());
+        uint check;
+        bob.DamagePlayer(50);
+        check = bob.GetHealth();
+        Assert.AreEqual(check, 50);
+    }
+}
