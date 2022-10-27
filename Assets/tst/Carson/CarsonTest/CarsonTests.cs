31e468e89dc2f2907488ed1fe41eb8e10ef6a113
Scribee <c1sloan6@gmail.com>
2022-10-26 21:45:12 -0700

    Fix naming conflicts

--- Assets/tst/Carson/CarsonTest/CarsonTests.cs
+++ Assets/tst/Carson/CarsonTest/CarsonTests.cs
@@ -10,17 +10,17 @@ public class CarsonTests : MonoBehaviour {
 	// Start the game in normal difficulty and test if a player was created
 	[Test]
 	public void spawnPlayer() {
-		GameManager.Instance.StartGame(1); // start the game
+		GameManager.Instance.startGame(1); // start the game
 		// Make sure the player getter works and the player reference isn't null
 		//Assert.AreNotEqual(null, GameManager.Instance.GetPlayer());
-		Assert.AreEqual(1, GameManager.Instance.GetRound());
+		Assert.AreEqual(1, GameManager.Instance.getRound());
 	}
 	
 	// Start the game in normal difficulty and test if the inital wave of dogs is created
 	[Test]
 	public void spawnDog() {
-		GameManager.Instance.StartGame(1);
-		int count = GameManager.Instance.EnemiesLeft();
+		GameManager.Instance.startGame(1);
+		int count = GameManager.Instance.enemiesLeft();
 		
 		Debug.Log(count);
 		Assert.AreEqual(3, count);
