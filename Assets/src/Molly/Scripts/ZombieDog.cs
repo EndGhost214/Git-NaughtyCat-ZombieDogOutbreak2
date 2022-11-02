fcd16940c11b4a0273e7e6aec8fdb6e5a79b789e
MollyEM <mollymeadows584@yahoo.com>
2022-11-02 16:18:01 -0700

    attack animation when touching player

--- Assets/src/Molly/Scripts/ZombieDog.cs
+++ Assets/src/Molly/Scripts/ZombieDog.cs
@@ -5,61 +5,85 @@ using UnityEngine;
 
 public class ZombieDog : Dog
 {
-    //serialized field for sound management class
-    [SerializeField]
-    private Rigidbody2D ZomDogRB;
     
-    //other variables
+    //will hold the position of the zombie dog
     private Vector3 pos;
 
+    //will hold the animation variables to be set
     private Animator animate;
 
-    //public AIPath aiPath;
+
+    private bool isAttacking = false;
    
     // default constructor
     public ZombieDog()
 	{
-        
     }
 
     //constructor that takes inital spawn position
     public ZombieDog(Vector3 pos)
 	{
         this.pos = pos;
-        //call dog noise sound from marissa's function
-        //SoundManager.Instance.ZombieSoundFunction();
     }
 
     // Start is called before the first frame update
     void Start()
     {
         //transform.position = pos;
+
+        //initiate sound upon spawning
         SoundManager.Instance.zombieSoundFunction();
+        //get the animator components in the object animate in order to set animations accordingly
         animate = gameObject.GetComponent<Animator>();
+
+        
+        animate.SetBool("isAttack", isAttacking);
+        //set the speed of the dog (will be dependent on the levelup class)
+        animate.SetFloat("Speed", speed);
     }
 
     // Update is called once per frame
+    void Update()
+    {
+        /*Vector2 moveDirection = gameObject.GetComponent<Rigidbody2D>().velocity;
+         if (moveDirection != Vector2.zero) 
+         {
+            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+         }*/
+        
+        //retrieves the coordinates of the direction the zombie dog is moving
+        /*float horizontal = Player.transform.position.x;
+        float vertical = Player.transform.position.y;
+        float rotationSpeed = 720f;
+
+        Vector2 moveDirection = new Vector2(horizontal,vertical);
+        transform.Translate(moveDirection * speed * Time.deltaTime, Space.World);
+
+        if (moveDirection != Vector2.zero) 
+         {
+            Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, moveDirection);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+         }
+*/
+
+    }
+
+    
     void FixedUpdate()
     {
-        animate.SetFloat("Speed", speed);
+        //set the speed and the health
+        
         animate.SetInteger("Health", health);
         
-
-        //if the dog is moving to the right, then flip it, visa versa for left
-        /*if(aiPath.desiredVelocity.x >= 0.01f){
-            transform.localScale = new Vector3(-1f,1f,1f);
-        }else if (aiPath.desiredVelocity.x <= -0.01f){
-            transform.localScale = new Vector3(1f,1f,1f);
-        }
-        */
-        //if the dog is moving down/up, flip it accordingly
+        
 
         //BOUNDARY TEST: GO TO DOG SCRIPT TO SEE THE DEATH FUNCTION
         //HEALTH IS SERIALIZED FIELD SO THAT YOU CAN CHANGE HEALTH AT RUN TIME
         if(health==0){
             //animate.SetInteger("Health", health);
-            gameObject.GetComponent<Animation>()["DeathAnim"].wrapMode = WrapMode.Once;
-            gameObject.GetComponent<Animation>().Play("DeathAnim");
+            //gameObject.GetComponent<Animation>()["DeathAnim"].wrapMode = WrapMode.Once;
+            //gameObject.GetComponent<Animation>().Play("DeathAnim");
             //animate.Play("DeathAnim");
             //Debug.Log("here");
             Death();
@@ -73,8 +97,9 @@ public class ZombieDog : Dog
 	{
         //Debug.Log("testing");
         if(collision.gameObject.tag == "Player"){
+            isAttacking = true;
             Debug.Log("player is in dog zone");
-            //set animate to bite
+            animate.SetBool("isAttack", isAttacking);
         }
         if(collision.gameObject.tag == "Bullet"){
             TakeDamage((int) collision.gameObject.GetComponent<Bullet>().getDamage());
@@ -82,9 +107,12 @@ public class ZombieDog : Dog
     }
 
     void OnCollisionExit2D(Collision2D collision)
-	{
+	{  
+        
         if(collision.gameObject.tag == "Player"){
+            isAttacking = false;
             Debug.Log("player exited dog zone");
+            animate.SetBool("isAttack", isAttacking);
         }
     }
 
