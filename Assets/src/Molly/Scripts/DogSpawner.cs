2e8fa8a077b19a5826acc4b0cb6959d0d21173eb
MollyEM <mollymeadows584@yahoo.com>
2022-11-14 21:59:48 -0800

    Dynamic Binding Video

--- Assets/src/Molly/Scripts/DogSpawner.cs
+++ Assets/src/Molly/Scripts/DogSpawner.cs
@@ -10,6 +10,13 @@ using UnityEngine;
 
 public class DogSpawner : MonoBehaviour
 {
+[SerializeField]
+private GameObject bossDog;
+
+    void Start(){
+        DogPool.Instance.SpawnFromDogPool("ZombieDog", gameObject.transform.position, Quaternion.identity);
+        Instantiate(bossDog, gameObject.transform.position, Quaternion.identity);
+    }
     void FixedUpdate(){
         //Debug.Log(gameObject.transform.position);
         if (Input.GetKeyDown(KeyCode.L))
